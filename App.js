8e1d6062f02d8028deb870666f3e5c50574b1e1c
sneider334 <130092912+sneider334@users.noreply.github.com>
2023-04-08 16:15:05 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import ItemListContainerr from "./Componentes/ItemListContainerr";
+import Header from "./Componentes/NavBar";
+
+
+function App() {
+return (
+<>
+    <Header/>
+    <ItemListContainerr greeting="Bienvenido a esta página"/>
+    
+</>)
+
+}
+
+export default App;
